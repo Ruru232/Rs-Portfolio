6a78db917ff653ceafd3bebf95bb4a5c86bd37af
Ruru232 <sanchez.rolando1998@yahoo.com>
2025-01-28 01:00:58 +0800

    feat: added About me page

---
+++ app/components/Certificates.tsx
@@ -0,0 +1,11 @@
+function Certificates() {
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center mx-[5%] 2xl:mx-[25%] mt-20 p-1 ">
+        <h2 className="text-2xl">Certificates</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Certificates;
--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
-import Navbar from './Navbar';
+import Certificates from './Certificates';
 
 export default function About() {
   return (
     <div>
-      <div className="flex flex-wrap justify-center mx-[5%] 2xl:mx-[25%] mt-28 p-5">
+      <div className="flex flex-wrap justify-center mx-[5%] 2xl:mx-[25%] mt-28 p-1 ">
         <div className="flex justify-center flex-col md:flex-row">
           <div className="mt-0 md:mt-2 w-[250px]">
             <Image
@@ -30,50 +30,82 @@ export default function About() {
         </div>
       </div>
       <div className="flex flex-wrap mx-[5%] 2xl:mx-[25%] p-5">
-        <div className="mt-5">
-          <h2 className="text-2xl font-bold mb-5">Work Experience:</h2>
-          <div className="space-y-4">
-            <div className="border-l-4 border-blue-500 pl-4">
-              <p className="font-semibold">
-                Web Developer Front-End Intern at Codebility
-              </p>
-              <p className="text-sm text-gray-600">
-                December 2, 2024 - Present
-              </p>
+        <div className="flex justify-center flex-col md:flex-row">
+          <div className="mt-5 ml-5 ">
+            <h2 className="text-2xl font-bold mb-5">Work Experience:</h2>
+            <div className="space-y-4">
+              <div className="border-l-4 border-blue-500 pl-4">
+                <p className="font-semibold">
+                  Web Developer Front-End Intern/Trainee at Codebility
+                </p>
+                <p className="text-sm text-gray-600">
+                  July 16, 2024 - December 2, 2024
+                </p>
+              </div>
+              <div className="border-l-4 border-blue-500 pl-4">
+                <p className="font-semibold">Freelance Computer Technician</p>
+                <p className="text-sm text-gray-600">
+                  May 2, 2024 - July 16, 2024
+                </p>
+              </div>
+              <div className="border-l-4 border-blue-500 pl-4">
+                <p className="font-semibold">
+                  I.T Technician at Hapson Technology
+                </p>
+                <p className="text-sm text-gray-600">
+                  February 05, 2024 - May 1, 2024
+                </p>
+              </div>
+              <div className="border-l-4 border-blue-500 pl-4">
+                <p className="font-semibold">
+                  Laptop Technician at Reer Global Philippines
+                </p>
+                <p className="text-sm text-gray-600">
+                  May 22, 2023 - August 22, 2023
+                </p>
+              </div>
+              <div className="border-l-4 border-blue-500 pl-4">
+                <p className="font-semibold">
+                  Web Developer Front-End Intern at Highly Succeed Inc
+                </p>
+                <p className="text-sm text-gray-600">
+                  March 14, 2022 - April 22, 2022
+                </p>
+              </div>
             </div>
-            <div className="border-l-4 border-blue-500 pl-4">
-              <p className="font-semibold">Freelance Computer Technician</p>
-              <p className="text-sm text-gray-600">
-                May 2, 2024 - July 16, 2024
-              </p>
-            </div>
-            <div className="border-l-4 border-blue-500 pl-4">
-              <p className="font-semibold">
-                I.T Technician at Hapson Technology
-              </p>
-              <p className="text-sm text-gray-600">
-                February 05, 2024 - May 1, 2024
-              </p>
-            </div>
-            <div className="border-l-4 border-blue-500 pl-4">
-              <p className="font-semibold">
-                Laptop Technician at Reer Global Philippines
-              </p>
-              <p className="text-sm text-gray-600">
-                May 22, 2023 - August 22, 2023
-              </p>
-            </div>
-            <div className="border-l-4 border-blue-500 pl-4">
-              <p className="font-semibold">
-                Web Developer Front-End Intern at Highly Succeed Inc
-              </p>
-              <p className="text-sm text-gray-600">
-                March 14, 2022 - April 22, 2022
-              </p>
+          </div>
+          <div className="mt-5 ml-5 ">
+            <h2 className="text-2xl font-bold mb-5">Education:</h2>
+            <div className="space-y-4">
+              <div className="border-l-4 border-blue-500 pl-4">
+                <p className="font-semibold">BS in Computer Science</p>
+                <p className="text-sm text-gray-600">
+                  2020-2024 at Pamantasan ng Lungsod ng Muntinlupa
+                </p>
+              </div>
+              <div className="border-l-4 border-blue-500 pl-4">
+                <p className="font-semibold">
+                  Associate in Computer Technology
+                </p>
+                <p className="text-sm text-gray-600">
+                  2018-2020 at Pamantasan ng Lungsod ng Muntinlupa
+                </p>
+              </div>
+
+              <div className="border-l-4 border-blue-500 pl-4">
+                <p className="font-semibold">
+                  Computer System, Programming and Network Services
+                </p>
+                <p className="text-sm text-gray-600">
+                  2016-2018 at Dr. Felimon C. Aguilar Information Technology
+                  Training Institute
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <Certificates />
     </div>
   );
 }
