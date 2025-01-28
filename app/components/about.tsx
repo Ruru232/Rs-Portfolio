03726f8d55a1cd78ddfd4375907d40791fd995a9
Ruru232 <sanchez.rolando1998@yahoo.com>
2025-01-28 16:53:58 +0800

    fix: deployment issue

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -28,9 +28,11 @@ export default function About() {
             </p>
             <div className="relative inline-block">
               <div className="absolute inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
-              <button className="relative px-7 py-4 bg-customBlack rounded-lg leading-none">
-                Download my CV
-              </button>
+              <a href="/" download>
+                <button className="relative px-7 py-4 bg-customBlack rounded-lg leading-none">
+                  Download my CV
+                </button>
+              </a>
             </div>
           </div>
         </div>
